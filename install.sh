f65b401e89bc52f2079ec2ddfe3fa0ba20740b57
Rajesh <rajeshrana5545@gmail.com>
2023-11-05 18:20:05 +0530

    fixed ruby spelling and file not found error

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 apt update
 apt install cowsay -y
-apt install rupy -y
+apt install ruby -y
 gem install lolcat
 apt install figlet -y
 if ![ -d ~/.termux ]; then
   echo "Directory not exists.\nCreating a ~/.termux directory." | lolcat
   mkdir ~/.termux
 fi
-cp motd.sh ~/.termux/motd.sh
-cp name ~/.termux/name
+cp ./motd.sh ~/.termux/motd.sh
+cp ./cowsay ~/.termux/cowsay
 
