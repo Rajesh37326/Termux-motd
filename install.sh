f542d76650ad06434fcda78e1196da7fe0c91e58
Rajesh <rajeshrana5545@gmail.com>
2023-11-05 18:34:35 +0530

    Fixing if condition.

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ apt install cowsay -y
 apt install ruby -y
 gem install lolcat
 apt install figlet -y
-if [! -d ~/.termux ]; then
+if [ ! -d ~/.termux ]; then
   echo "Directory not exists.\nCreating a ~/.termux directory." | lolcat
   mkdir ~/.termux
 fi
-cp ./motd.sh ~/.termux/motd.sh
-cp ./cowsay ~/.termux/cowsay
+mv ./motd.sh ~/.termux/motd.sh
+mv ./cowsay ~/.termux/cowsay
 
